e4e3daa3c1e69a797dbe935e7f1b3d2defab53cb
sarablom <sara.cecilia.blom@gmail.com>
2021-12-31 08:56:03 +0100

    add placeholder styling

--- src/components/IconInput/IconInput.js
+++ src/components/IconInput/IconInput.js
@@ -10,14 +10,14 @@ const STYLES = {
   small: {
     fontSize: 14,
     top: 0,
-    size: 24
+    size: 24,
   },
   large: {
     fontSize: 18,
     top: 0,
-    size: 28
-  }
-}
+    size: 28,
+  },
+};
 
 const IconInput = ({ label, icon, width = 250, size, placeholder }) => {
   const styles = STYLES[size];
@@ -27,25 +27,44 @@ const IconInput = ({ label, icon, width = 250, size, placeholder }) => {
   }
 
   return (
-      <SearchBar size={size} style={{ "--width": width + 'px' }}>
-        <Icon id={icon} size={styles.size} style={{ position: "absolute", top: styles.top + 'px' }}></Icon>
-        <VisuallyHidden>{placeholder}</VisuallyHidden>
-        <InputElement type="text" placeholder={placeholder} style={{ "--fontSize": styles.fontSize + 'px' }} />
-      </SearchBar>
+    <SearchBar size={size} style={{ "--width": width + "px" }}>
+      <Icon
+        id={icon}
+        size={styles.size}
+        style={{
+          position: "absolute",
+          top: styles.top + "px",
+          color: COLORS.gray700,
+        }}
+      ></Icon>
+      <VisuallyHidden>{label}</VisuallyHidden>
+      <InputElement
+        type="text"
+        placeholder={placeholder}
+        style={{ "--fontSize": styles.fontSize + "px" }}
+      />
+    </SearchBar>
   );
 };
 
-const SearchBar = styled.div `
+const SearchBar = styled.div`
   position: relative;
   width: var(--width);
-`
+`;
 
 const InputElement = styled.input`
   font-family: inherit;
   font-size: var(--fontSize);
+  font-weight: 700;
   border: none;
   border-bottom: 2px solid ${COLORS.black};
   padding: 4px 10px 4px 36px;
+  outline-offset: 2px;
+
+  &::placeholder {
+    color: ${COLORS.gray500};
+    font-weight: 400;
+  }
 `;
 
 export default IconInput;
